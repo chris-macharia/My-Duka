236aa2a7d8f4c472ee5b83ce358f338744e4372c
chris <christophermaina003@gmail.com>
2025-09-11 00:26:09 +0300

    Fix App.xaml.cs for the Obsolete MainPage setter error.

--- DukaInventory/App.xaml.cs
+++ DukaInventory/App.xaml.cs
@@ -8,20 +8,21 @@ namespace DukaInventory;
 
 public partial class App : Application
 {
-	public static DatabaseService Database { get; private set; }
+    public static DatabaseService Database { get; private set; }
 
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		string dbPath = Path.Combine(FileSystem.AppDataDirectory, "duka.db3");
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "duka.db3");
         Database = new DatabaseService(dbPath);
+    }
 
-    	MainPage = new NavigationPage(new MainPage());
-	}
-
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
-	}
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // Wrap MainPage in a NavigationPage if you want navigation support
+        return new Window(new NavigationPage(new MainPage()));
+        // OR if you're using Shell, replace with:
+        // return new Window(new AppShell());
+    }
 }
