6dad54d529913668b1fd594002aac22ee2bff3d4
chris <christophermaina003@gmail.com>
2025-09-10 23:09:38 +0300

    Build the DB path and create DatabaseService.

--- DukaInventory/App.xaml.cs
+++ DukaInventory/App.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace DukaInventory;
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+using DukaInventory.Data;
+using Microsoft.Maui;
+
+namespace DukaInventory;
 
 public partial class App : Application
 {
+	public static DatabaseService Database { get; private set; }
+
 	public App()
 	{
 		InitializeComponent();
+
+		string dbPath = Path.Combine(FileSystem.AppDataDirectory, "duka.db3");
+        Database = new DatabaseService(dbPath);
+
+    	MainPage = new NavigationPage(new MainPage());
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
